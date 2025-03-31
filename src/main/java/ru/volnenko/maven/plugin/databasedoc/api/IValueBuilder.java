719290afd48495b33f786a006e3243ae9c66cfd5
Denis Volnenko <denis@volnenko.ru>
2025-03-31 12:27:19 +0400

    Project updated.

--- src/main/java/ru/volnenko/maven/plugin/databasedoc/api/IValueBuilder.java
+++ src/main/java/ru/volnenko/maven/plugin/databasedoc/api/IValueBuilder.java
@@ -1,5 +1,4 @@
 package ru.volnenko.maven.plugin.databasedoc.api;
 
 public interface IValueBuilder extends IRootBuilder {
-
 }
---
+++ src/main/java/ru/volnenko/maven/plugin/databasedoc/generator/ColumnGenerator.java
@@ -0,0 +1,14 @@
+package ru.volnenko.maven.plugin.databasedoc.generator;
+
+import lombok.NonNull;
+import ru.volnenko.maven.plugin.databasedoc.model.Column;
+
+public class ColumnGenerator extends AbstractModelGenerator<Column> {
+
+    @NonNull
+    @Override
+    public String generate(@NonNull Column model, @NonNull Integer index) {
+        return null;
+    }
+
+}
