56ba757bede231ee7052661202723c17671ff456
Denis Volnenko <denis@volnenko.ru>
2025-03-31 17:07:48 +0400

    Project updated.

--- src/main/java/ru/volnenko/maven/plugin/databasedoc/api/ICreateTableGenerator.java
+++ src/main/java/ru/volnenko/maven/plugin/databasedoc/api/ICreateTableGenerator.java
@@ -1,4 +1,18 @@
 package ru.volnenko.maven.plugin.databasedoc.api;
 
-public interface ICreateTableGenerator {
+import lombok.NonNull;
+import ru.volnenko.maven.plugin.databasedoc.generator.CreateTableGenerator;
+import ru.volnenko.maven.plugin.databasedoc.model.CreateTable;
+
+public interface ICreateTableGenerator extends IGenerator {
+
+    @NonNull
+    CreateTableGenerator createTable(@NonNull CreateTable createTable);
+
+    @NonNull
+    CreateTableGenerator serviceName(@NonNull String serviceName);
+
+    @NonNull
+    CreateTableGenerator dataBaseInfo(@NonNull String dataBaseInfo);
+
 }
