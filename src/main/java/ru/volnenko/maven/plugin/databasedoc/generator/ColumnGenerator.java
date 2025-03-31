5f1ed4ae65537481ee3291deb13d994cba8acff2
Denis Volnenko <denis@volnenko.ru>
2025-03-31 12:41:32 +0400

    Project updated.

--- src/main/java/ru/volnenko/maven/plugin/databasedoc/generator/ColumnGenerator.java
+++ src/main/java/ru/volnenko/maven/plugin/databasedoc/generator/ColumnGenerator.java
@@ -5,8 +5,37 @@ import ru.volnenko.maven.plugin.databasedoc.model.Column;
 
 public final class ColumnGenerator extends AbstractGenerator {
 
+    @NonNull
+    private StringBuilder stringBuilder = new StringBuilder();
+
+    @NonNull
+    private Integer index = 1;
+
+    @NonNull
+    private Column column = new Column();
+
+    @NonNull
+    public ColumnGenerator stringBuilder(@NonNull final StringBuilder stringBuilder) {
+        this.stringBuilder = stringBuilder;
+        return this;
+    }
+
+    @NonNull
+    public ColumnGenerator index(@NonNull final Integer index) {
+        this.index = index;
+        return this;
+    }
+
+    @NonNull
+    public ColumnGenerator value(@NonNull final Column column) {
+        this.column = column;
+        return this;
+    }
+
+    @NonNull
     @Override
-    public @NonNull String generate() {
+    public String generate() {
         return null;
     }
+
 }
