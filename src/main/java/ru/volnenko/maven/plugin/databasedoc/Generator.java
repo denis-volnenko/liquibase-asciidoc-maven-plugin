d365c0d23ad9f6609c5f244c2a3bf2c9fd528289
Denis Volnenko <denis@volnenko.ru>
2025-03-31 18:47:46 +0400

    Project updated.

--- src/main/java/ru/volnenko/maven/plugin/databasedoc/Generator.java
+++ src/main/java/ru/volnenko/maven/plugin/databasedoc/Generator.java
@@ -8,8 +8,6 @@ import net.sourceforge.plantuml.FileFormat;
 import net.sourceforge.plantuml.FileFormatOption;
 import net.sourceforge.plantuml.SourceStringReader;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
