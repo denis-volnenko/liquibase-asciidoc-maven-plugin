3b41fb6e84f4dfc731e5b05b8df9fda28e49a7d3
Denis Volnenko <denis@volnenko.ru>
2025-04-02 09:50:28 +0400

    Project updated.

--- git-pull-push.sh
+++ git-pull-push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 git pull
-git pull git@github.com:denis-volnenko/maven-liquilbase-asciidoc-plugin.git
+git pull git@github.com:denis-volnenko/liquilbase-asciidoc-maven-plugin.git
 
 git add .
 git commit -m "Project updated."
 
 git push
 
-git push git@github.com:denis-volnenko/maven-liquilbase-asciidoc-plugin.git
+git push git@github.com:denis-volnenko/liquilbase-asciidoc-maven-plugin.git
