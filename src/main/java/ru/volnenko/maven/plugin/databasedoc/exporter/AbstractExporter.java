33e295d30302d903ec3d178e9c32d556fc61fde6
Denis Volnenko <denis@volnenko.ru>
2025-04-04 19:41:58 +0400

    Project updated.

--- src/main/java/ru/volnenko/maven/plugin/databasedoc/exporter/AbstractExporter.java
+++ src/main/java/ru/volnenko/maven/plugin/databasedoc/exporter/AbstractExporter.java
@@ -1,8 +1,16 @@
 package ru.volnenko.maven.plugin.databasedoc.exporter;
 
+import lombok.NonNull;
+
 public abstract class AbstractExporter implements IExporter {
 
     private boolean enable = false;
 
+    @NonNull
+    @Override
+    public IExporter enable(boolean enable) {
+        this.enable = enable;
+        return this;
+    }
 
 }
--- src/main/java/ru/volnenko/maven/plugin/databasedoc/exporter/IExporter.java
+++ src/main/java/ru/volnenko/maven/plugin/databasedoc/exporter/IExporter.java
@@ -1,4 +1,10 @@
 package ru.volnenko.maven.plugin.databasedoc.exporter;
 
+import lombok.NonNull;
+
 public interface IExporter {
+
+    @NonNull
+    IExporter enable(boolean enable);
+
 }
