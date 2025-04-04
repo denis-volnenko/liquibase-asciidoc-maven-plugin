6a4efd71d846c2ffb56a249a8f1051e846822589
Denis Volnenko <denis@volnenko.ru>
2025-04-04 19:40:56 +0400

    Project updated.

--- src/main/java/ru/volnenko/maven/plugin/databasedoc/exporter/Exporters.java
+++ src/main/java/ru/volnenko/maven/plugin/databasedoc/exporter/Exporters.java
@@ -1,37 +1,59 @@
 package ru.volnenko.maven.plugin.databasedoc.exporter;
 
+import lombok.Getter;
 import lombok.NonNull;
+import ru.volnenko.maven.plugin.databasedoc.exporter.impl.*;
 
 public final class Exporters implements IExporters {
+
+    @Getter
+    @NonNull
+    private final IExporter adoc = new ExporterAdoc();
+
+    @Getter
+    @NonNull
+    private final IExporter svg = new ExporterSvg();
+
+    @Getter
+    @NonNull
+    private final IExporter json = new ExporterJson();
+
+    @Getter
+    @NonNull
+    private final IExporter yaml = new ExporterYaml();
+
+    @Getter
+    @NonNull
+    private final IExporter puml = new ExporterPuml();
 
     @NonNull
     @Override
     public IExporter adoc() {
-        return null;
+        return adoc;
     }
 
     @NonNull
     @Override
     public IExporter svg() {
-        return null;
+        return svg;
     }
 
     @NonNull
     @Override
     public IExporter json() {
-        return null;
+        return json;
     }
 
     @NonNull
     @Override
     public IExporter yaml() {
-        return null;
+        return yaml;
     }
 
     @NonNull
     @Override
     public IExporter puml() {
-        return null;
+        return puml;
     }
 
 }
