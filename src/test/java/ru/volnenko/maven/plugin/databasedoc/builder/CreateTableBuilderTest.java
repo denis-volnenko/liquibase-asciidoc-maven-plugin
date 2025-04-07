a8ed2c03b3c2fcf9ac0ad9d3cdb811bcab7ee629
Pavel Forinnyy <forinnyy@gmail.com>
2025-04-07 18:53:36 +0400

    Tests updated.

--- src/test/java/ru/volnenko/maven/plugin/databasedoc/builder/CreateTableBuilderTest.java
+++ src/test/java/ru/volnenko/maven/plugin/databasedoc/builder/CreateTableBuilderTest.java
@@ -3,10 +3,11 @@ package ru.volnenko.maven.plugin.databasedoc.builder;
 import org.junit.Assert;
 import org.junit.Test;
 import ru.volnenko.maven.plugin.databasedoc.builder.impl.*;
+import ru.volnenko.maven.plugin.databasedoc.model.impl.CreateTable;
 
 public class CreateTableBuilderTest {
 
-    CreateTableBuilder createTableBuilder = RootBuilder.create()
+    CreateTableBuilder table = RootBuilder.create()
             .dsl()
             .changeSet()
             .add()
@@ -15,14 +16,33 @@ public class CreateTableBuilderTest {
 
     @Test
     public void testNotNull() {
-        Assert.assertNotNull(createTableBuilder.root());
-        Assert.assertNotNull(createTableBuilder.change());
-        Assert.assertNotNull(createTableBuilder.column());
-        Assert.assertNotNull(createTableBuilder.createTable());
-        Assert.assertNotNull(createTableBuilder.tablespace(""));
-        Assert.assertNotNull(createTableBuilder.catalogName(""));
-        Assert.assertNotNull(createTableBuilder.tableName(""));
-        Assert.assertNotNull(createTableBuilder.remarks(""));
+        Assert.assertNotNull(table.root());
+        Assert.assertNotNull(table.change());
+        Assert.assertNotNull(table.column());
+        Assert.assertNotNull(table.createTable());
+        Assert.assertNotNull(table.tablespace(""));
+        Assert.assertNotNull(table.catalogName(""));
+        Assert.assertNotNull(table.tableName(""));
+        Assert.assertNotNull(table.remarks(""));
+    }
+
+    @Test
+    public void testTable() {
+        table.tableName("Table name");
+        table.catalogName("Catalog name");
+        table.tablespace("Table space");
+        table.remarks("Table remarks");
+        CreateTable createTable = table.root()
+                .getDatabaseChangeLog()
+                .getChangeSet()
+                .get(0)
+                .getChanges()
+                .get(0)
+                .getCreateTable();
+        Assert.assertEquals("Table name", createTable.getTableName());
+        Assert.assertEquals("Catalog name", createTable.getCatalogName());
+        Assert.assertEquals("Table space", createTable.getTablespace());
+        Assert.assertEquals("Table remarks", createTable.getRemarks());
     }
 
 }
--- src/test/java/ru/volnenko/maven/plugin/databasedoc/builder/CreateTypeBuilderTest.java
+++ src/test/java/ru/volnenko/maven/plugin/databasedoc/builder/CreateTypeBuilderTest.java
@@ -7,13 +7,14 @@ import ru.volnenko.maven.plugin.databasedoc.model.impl.CreateType;
 
 public class CreateTypeBuilderTest {
 
-//    @Test
-//    public void testNotNull() {
-//        CreateTypeBuilder createTypeBuilder = RootBuilder.create()
+//    CreateTypeBuilder createTypeBuilder = RootBuilder.create()
 //                .dsl()
 //                .changeSet()
 //                .add()
 //                .change();
+
+//    @Test
+//    public void testNotNull() {
 //        Assert.assertNotNull(createTypeBuilder.root());
 //        Assert.assertNotNull(createTypeBuilder.change());
 //        Assert.assertNotNull(createTypeBuilder.catalogName(""));
