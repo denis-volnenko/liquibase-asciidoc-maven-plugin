caff957e268993c3ffeb8f1fc15d952fe6d84489
Denis Volnenko <denis@volnenko.ru>
2025-04-08 12:56:51 +0400

    tm-sql-to-asciidoc updated.

---
+++ example/tm-sql-to-asciidoc/src/main/resources/changelog/03-create-user.sql
@@ -0,0 +1,6 @@
+app_task (
+    `id` VARCHAR(255) DEFAULT '' NOT NULL,
+    `username` VARCHAR(255) DEFAULT '' NOT NULL,
+    `password_hash` VARCHAR(255) DEFAULT '' NOT NULL,
+    CONSTRAINT pk_app_user PRIMARY KEY (id)
+);
