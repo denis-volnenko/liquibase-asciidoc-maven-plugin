d17a8089be98fc4f3bda703c5848c029bc122b9e
Denis Volnenko <denis@volnenko.ru>
2025-04-08 21:01:01 +0400

    Project updated.

--- src/main/java/ru/volnenko/maven/plugin/databasedoc/generator/IEntityRelationDiagramDocumentGenerator.java
+++ src/main/java/ru/volnenko/maven/plugin/databasedoc/generator/IEntityRelationDiagramDocumentGenerator.java
@@ -9,7 +9,8 @@ import java.util.List;
 
 public interface IEntityRelationDiagramDocumentGenerator extends IGenerator {
 
-    @NonNull EntityRelationDiagramDocumentGenerator erdType(@NonNull ErdType erdType);
+    @NonNull
+    EntityRelationDiagramDocumentGenerator erdType(@NonNull ErdType erdType);
 
     @NonNull
     ErdType erdType();
