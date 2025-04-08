36859367b01f4191be48eea21a9965b9dd641e79
Denis Volnenko <denis@volnenko.ru>
2025-04-08 13:19:06 +0400

    tm-sql-to-asciidoc updated.

--- example/tm-sql-to-asciidoc/src/main/resources/changelog/01-create-project.sql
+++ example/tm-sql-to-asciidoc/src/main/resources/changelog/01-create-project.sql
@@ -4,3 +4,8 @@ CREATE TABLE app_project (
     description VARCHAR(255) DEFAULT '' NOT NULL,
     CONSTRAINT pk_app_project PRIMARY KEY (id)
 );
+
+comment on table app_project is 'Проект';
+comment on column app_project.id is 'Идентификатор';
+comment on column app_project.name is 'Название';
+comment on column app_project.description is 'Описание';
