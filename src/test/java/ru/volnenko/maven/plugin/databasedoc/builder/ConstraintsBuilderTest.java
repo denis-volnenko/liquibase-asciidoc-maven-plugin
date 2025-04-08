646424cb4329ad9a62092598b31be0d4df120d21
Pavel Forinnyy <forinnyy@gmail.com>
2025-04-08 11:30:31 +0400

    Tests updated.

--- src/test/java/ru/volnenko/maven/plugin/databasedoc/builder/ConstraintsBuilderTest.java
+++ src/test/java/ru/volnenko/maven/plugin/databasedoc/builder/ConstraintsBuilderTest.java
@@ -3,6 +3,7 @@ package ru.volnenko.maven.plugin.databasedoc.builder;
 import org.junit.Assert;
 import org.junit.Test;
 import ru.volnenko.maven.plugin.databasedoc.builder.impl.*;
+import ru.volnenko.maven.plugin.databasedoc.model.impl.Constraints;
 
 public class ConstraintsBuilderTest {
 
@@ -27,6 +28,31 @@ public class ConstraintsBuilderTest {
         Assert.assertNotNull(constraintsBuilder.unique(true));
         Assert.assertNotNull(constraintsBuilder.uniqueConstraintName(""));
         Assert.assertNotNull(constraintsBuilder.foreignKeyName(""));
+    }
+
+    @Test
+    public void testReturn() {
+        constraintsBuilder.foreignKeyName("Constraint name");
+        constraintsBuilder.uniqueConstraintName("Unique constraint name");
+        constraintsBuilder.primaryKey(true);
+        constraintsBuilder.nullable(true);
+        constraintsBuilder.unique(true);
+        Constraints constraints = constraintsBuilder.root()
+                .getDatabaseChangeLog()
+                .getChangeSet()
+                .get(0)
+                .getChanges()
+                .get(0)
+                .getCreateTable()
+                .getColumns()
+                .get(0)
+                .getColumn()
+                .getConstraints();
+        Assert.assertEquals("Constraint name", constraints.getForeignKeyName());
+        Assert.assertEquals("Unique constraint name", constraints.getUniqueConstraintName());
+        Assert.assertEquals(true, constraints.getPrimaryKey());
+        Assert.assertEquals(true, constraints.getNullable());
+        Assert.assertEquals(true, constraints.getUnique());
     }
 
 }
