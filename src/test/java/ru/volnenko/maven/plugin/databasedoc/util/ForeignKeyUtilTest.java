6d31b2bfcdc76fbc014860879d9c12a086b21b40
Pavel Forinnyy <forinnyy@gmail.com>
2025-04-10 15:13:55 +0400

    Tests updated.

---
+++ src/test/java/ru/volnenko/maven/plugin/databasedoc/util/ForeignKeyUtilTest.java
@@ -0,0 +1,4 @@
+package ru.volnenko.maven.plugin.databasedoc.util;
+
+public class ForeignKeyUtilTest {
+}
--- src/test/java/ru/volnenko/maven/plugin/databasedoc/util/MapperUtilTest.java
+++ src/test/java/ru/volnenko/maven/plugin/databasedoc/util/MapperUtilTest.java
@@ -1,15 +1,11 @@
 package ru.volnenko.maven.plugin.databasedoc.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import lombok.NonNull;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MapperUtilTest {
-
-    @NonNull
-    private final MapperUtil mapperUtil = new MapperUtil();
 
     @NonNull
     private static final ObjectMapper JSON = MapperUtil.json();
