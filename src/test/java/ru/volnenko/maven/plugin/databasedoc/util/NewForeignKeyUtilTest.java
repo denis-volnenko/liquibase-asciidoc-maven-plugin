f6a5eed5ec8c4b750332c4ed2e4d71f9aaac160c
Pavel Forinnyy <forinnyy@gmail.com>
2025-04-12 13:45:12 +0400

    Tests updated.

--- src/test/java/ru/volnenko/maven/plugin/databasedoc/util/NewForeignKeyUtilTest.java
+++ src/test/java/ru/volnenko/maven/plugin/databasedoc/util/NewForeignKeyUtilTest.java
@@ -3,6 +3,8 @@ package ru.volnenko.maven.plugin.databasedoc.util;
 
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -15,30 +17,40 @@ import ru.volnenko.maven.plugin.databasedoc.model.impl.Column;
 public class NewForeignKeyUtilTest {
 
     @Test
+    @DisplayName("ForeignKeyUtil метод fk с параметром AddForeignKeyConstraint")
+    @Description("Проверка метода fk с параметром AddForeignKeyConstraint на возврат корректного объекта FK")
     @UseDataProvider(value = "validForeignKeyConstraints", location = ForeignKeyUtilData.class)
     public void testValidConstraintsCorrectReturn(AddForeignKeyConstraint constraint) {
         Assert.assertEquals(ForeignKeyUtilData.correctReturnOfFkConstraintMethod(), ForeignKeyUtil.fk(constraint));
     }
 
     @Test
+    @DisplayName("ForeignKeyUtil метод fk с параметром AddForeignKeyConstraint")
+    @Description("Проверка метода fk с параметром AddForeignKeyConstraint на NotNull")
     @UseDataProvider(value = "validForeignKeyConstraints", location = ForeignKeyUtilData.class)
     public void testValidConstraints(AddForeignKeyConstraint constraint) {
         Assert.assertNotNull(ForeignKeyUtil.fk(constraint));
     }
 
     @Test
+    @DisplayName("ForeignKeyUtil метод fk с параметром AddForeignKeyConstraint")
+    @Description("Проверка метода fk с параметром AddForeignKeyConstraint на Null")
     @UseDataProvider(value = "invalidForeignKeyConstraints", location = ForeignKeyUtilData.class)
     public void testInvalidConstraints(AddForeignKeyConstraint constraint) {
         Assert.assertNull(ForeignKeyUtil.fk(constraint));
     }
 
     @Test
+    @DisplayName("ForeignKeyUtil метод enabled с параметром Column")
+    @Description("Проверка метода fk с параметром Column на возврат true")
     @UseDataProvider(value = "trueColumns", location = ForeignKeyUtilData.class)
     public void testValidColumns(Column column) {
         Assert.assertTrue(ForeignKeyUtil.enabled(column));
     }
 
     @Test
+    @DisplayName("ForeignKeyUtil метод enabled с параметром Column")
+    @Description("Проверка метода fk с параметром Column на возврат false")
     @UseDataProvider(value = "falseColumns", location = ForeignKeyUtilData.class)
     public void testInvalidColumns(Column column) {
         Assert.assertFalse(ForeignKeyUtil.enabled(column));
