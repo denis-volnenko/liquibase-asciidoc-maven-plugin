04594cb28e641de7b7a28f6d907a2b7acc27cc2c
Pavel Forinnyy <forinnyy@gmail.com>
2025-04-14 15:02:37 +0400

    Tests updated.

--- src/test/java/ru/volnenko/maven/plugin/databasedoc/data/ForeignKeyUtilData.java
+++ src/test/java/ru/volnenko/maven/plugin/databasedoc/data/ForeignKeyUtilData.java
@@ -7,6 +7,7 @@ import lombok.NonNull;
 import ru.volnenko.maven.plugin.databasedoc.model.impl.AddForeignKeyConstraint;
 import ru.volnenko.maven.plugin.databasedoc.model.impl.Column;
 import ru.volnenko.maven.plugin.databasedoc.model.impl.FK;
+import ru.volnenko.maven.plugin.databasedoc.util.MapperUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -48,79 +49,59 @@ public class ForeignKeyUtilData {
         return fk;
     }
 
-    private static InputStream inputStreamFromJsonFile(String path) {
-        try {
-            final InputStream inputStream = ForeignKeyUtilData.class
-                    .getClassLoader()
-                    .getResourceAsStream(path);
-            if (inputStream == null) {
-                throw new RuntimeException();
-            }
-            return inputStream;
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to load test data from " + path, e);
-        }
-    }
-
     @DataProvider
-    public static Object[][] validColumnWithTableName() throws IOException {
-        @NonNull final String path = "testdata/validColumnWithTableName.json";
-        @NonNull final InputStream inputStream = ForeignKeyUtilData.inputStreamFromJsonFile(path);
-        @NonNull final ObjectMapper objectMapper = new ObjectMapper();
-        @NonNull final TestData testData = objectMapper.readValue(inputStream, TestData.class);
+    public static Object[][] validColumnWithTableName() {
+        @NonNull final TestData testData = MapperUtil.parseJsonFromResource(
+                "testdata/validColumnWithTableName.json", TestData.class
+        );
         return testData.testCases.stream()
                 .map(testCase -> new Object[]{testCase.tableName, testCase.column})
                 .toArray(Object[][]::new);
     }
 
     @DataProvider
-    public static Object[][] invalidColumnWithTableName() throws IOException {
-        @NonNull final String path = "testdata/invalidColumnWithTableName.json";
-        @NonNull final InputStream inputStream = ForeignKeyUtilData.inputStreamFromJsonFile(path);
-        @NonNull final ObjectMapper objectMapper = new ObjectMapper();
-        @NonNull final TestData testData = objectMapper.readValue(inputStream, TestData.class);
+    public static Object[][] invalidColumnWithTableName() {
+        @NonNull final TestData testData = MapperUtil.parseJsonFromResource(
+                "testdata/invalidColumnWithTableName.json", TestData.class
+        );
         return testData.testCases.stream()
                 .map(testCase -> new Object[]{testCase.tableName, testCase.column})
                 .toArray(Object[][]::new);
     }
 
     @DataProvider
-    public static Object[] validForeignKeyConstraints() throws IOException {
-        @NonNull final String path = "testdata/validForeignKeyConstraints.json";
-        @NonNull final InputStream inputStream = ForeignKeyUtilData.inputStreamFromJsonFile(path);
-        @NonNull final ObjectMapper objectMapper = new ObjectMapper();
-        @NonNull final AddForeignKeyConstraint constraint = objectMapper.readValue(inputStream, AddForeignKeyConstraint.class);
+    public static Object[] validForeignKeyConstraints() {
+        @NonNull final AddForeignKeyConstraint constraint = MapperUtil.parseJsonFromResource(
+                "testdata/validForeignKeyConstraints.json", AddForeignKeyConstraint.class
+        );
         return new Object[][]{{constraint}};
     }
 
     @DataProvider
-    public static Object[][] invalidForeignKeyConstraints() throws IOException {
-        @NonNull final String path = "testdata/invalidForeignKeyConstraints.json";
-        @NonNull final InputStream inputStream = ForeignKeyUtilData.inputStreamFromJsonFile(path);
-        @NonNull final ObjectMapper mapper = new ObjectMapper();
-        @NonNull final AddForeignKeyConstraint[] constraints = mapper.readValue(inputStream, AddForeignKeyConstraint[].class);
+    public static Object[][] invalidForeignKeyConstraints() {
+        @NonNull final AddForeignKeyConstraint[] constraints = MapperUtil.parseJsonFromResource(
+                "testdata/invalidForeignKeyConstraints.json", AddForeignKeyConstraint[].class
+        );
         return Arrays.stream(constraints)
                 .map(constraint -> new Object[]{constraint})
                 .toArray(Object[][]::new);
     }
 
     @DataProvider
-    public static Object[][] trueColumns() throws IOException {
-        @NonNull final String path = "testdata/trueColumns.json";
-        @NonNull final InputStream inputStream = ForeignKeyUtilData.inputStreamFromJsonFile(path);
-        @NonNull final ObjectMapper objectMapper = new ObjectMapper();
-        @NonNull final Column[] columns = objectMapper.readValue(inputStream, Column[].class);
+    public static Object[][] trueColumns() {
+        @NonNull final Column[] columns = MapperUtil.parseJsonFromResource(
+                "testdata/trueColumns.json", Column[].class
+        );
         return Arrays.stream(columns)
                 .map(column -> new Object[]{column})
                 .toArray(Object[][]::new);
     }
 
     @DataProvider
-    public static Object[][] falseColumns() throws IOException {
-        @NonNull final String path = "testdata/falseColumns.json";
-        @NonNull final InputStream inputStream = ForeignKeyUtilData.inputStreamFromJsonFile(path);
-        @NonNull final ObjectMapper objectMapper = new ObjectMapper();
-        @NonNull final Column[] columns = objectMapper.readValue(inputStream, Column[].class);
+    public static Object[][] falseColumns() {
+        @NonNull final Column[] columns = MapperUtil.parseJsonFromResource(
+                "testdata/falseColumns.json", Column[].class
+        );
         return Arrays.stream(columns)
                 .map(column -> new Object[]{column})
                 .toArray(Object[][]::new);
